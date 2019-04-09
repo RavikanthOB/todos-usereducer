1b655f930bf081604f3287250cec7b722b1ea4f0
RavikanthOB <ravikanth.ob@gmail.com>
2019-04-09 13:00:33 +0530

    1 file modified, 1 added

--- index.js
+++ index.js
@@ -3,43 +3,15 @@ import { render } from 'react-dom';
 
 import './style.css';
 import * as constants from './constants';
-
+import {todoReducer} from './todoReducer';
 const initialState = [...constants.TODOS];
 const initialState = [
   {id: 1, name: 'Get started', complete: false}
 ];
-const todoReducer = (state, action) => {
-  switch (action.type) {
-    case 'ADD_TODO': {
-      
-      return (action.name.length)
-        ? [...state, {
-          id: state.length ? Math.max(...state.map(todo => todo.id)) + 1 : 0,
-          name: action.name,
-          complete: false
-        }]
-        : state;
-    }
-    case 'TOGGLE_COMPLETE': {
-  return state.map((item) =>
-    item.id === action.id
-      ? { ...item, complete: !item.complete }
-      : item
-  )
-}
-case 'DELETE_TODO': {
-  return state.filter((x) => x.id !== action.id);
-}
-case 'CLEAR_TODOS': {
-  return [];
-}
-    default: {
-      return state;
-    };
-  }
-}
+
 const Todo = () => {
   const inputRef = useRef();
+
 
 const [todos, dispatch] = useReducer(todoReducer, initialState);
 function addTodo(event) {
